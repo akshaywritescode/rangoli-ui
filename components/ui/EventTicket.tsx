e25a12873960fbf247b0b25752829f490e201403
Akshay <teamakshayyadav@gmail.com>
2026-09-14 17:51:28 +0530

    fix: improve perforation design with smaller circles
    - Reduce circles to 2x2 (w-2 h-2) for subtle effect
    - Increase count to 20 for even distribution
    - Use justify-evenly for better spacing
    - Move dashed line rendering order
    - Circles now properly cut through the line

--- components/ui/EventTicket.tsx
+++ components/ui/EventTicket.tsx
@@ -56,17 +56,17 @@ export default function EventTicket({
 
           {/* Perforated Divider with Semi-circles */}
           <div className="relative w-0">
+            {/* Dashed line */}
+            <div className="absolute top-0 bottom-0 left-0 border-l border-dashed border-white/20"></div>
             {/* Semi-circles cutting through */}
-            <div className="absolute top-0 bottom-0 -left-1.5 w-3 flex flex-col justify-around z-20">
-              {Array.from({ length: 14 }).map((_, i) => (
+            <div className="absolute top-0 bottom-0 -left-1 w-2 flex flex-col justify-evenly">
+              {Array.from({ length: 20 }).map((_, i) => (
                 <div
                   key={i}
-                  className="w-3 h-3 rounded-full bg-black"
+                  className="w-2 h-2 rounded-full bg-black"
                 />
               ))}
             </div>
-            {/* Dashed line behind semi-circles */}
-            <div className="absolute top-0 bottom-0 left-0 w-px border-l border-dashed border-white/10 z-10"></div>
           </div>
 
           {/* Right Side - Barcode Style */}
--- lib/components.ts
+++ lib/components.ts
@@ -70,14 +70,14 @@ export default function EventTicket({
           </div>
 
           <div className="relative w-0">
+            {/* Dashed line */}
+            <div className="absolute top-0 bottom-0 left-0 border-l border-dashed border-white/20"></div>
             {/* Semi-circles cutting through */}
-            <div className="absolute top-0 bottom-0 -left-1.5 w-3 flex flex-col justify-around z-20">
-              {Array.from({ length: 14 }).map((_, i) => (
-                <div key={i} className="w-3 h-3 rounded-full bg-black" />
+            <div className="absolute top-0 bottom-0 -left-1 w-2 flex flex-col justify-evenly">
+              {Array.from({ length: 20 }).map((_, i) => (
+                <div key={i} className="w-2 h-2 rounded-full bg-black" />
               ))}
             </div>
-            {/* Dashed line behind semi-circles */}
-            <div className="absolute top-0 bottom-0 left-0 w-px border-l border-dashed border-white/10 z-10"></div>
           </div>
 
           <div className="w-64 relative bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 p-8">
